03b6dbb0f01363075ca7fb4522d365e08f27bb1d
zodieth <contact.prodzodi@gmail.com>
2023-04-23 20:53:12 -0300

    server

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const { parse } = require("url");
 const next = require("next");
 
 const dev = process.env.NODE_ENV !== "production";
-const hostname = "localhost";
+const hostname = "alubric-client.something.alubric.com";
 const port = 3000;
 // when using middleware `hostname` and `port` must be provided below
 const app = next({ dev, hostname, port });
